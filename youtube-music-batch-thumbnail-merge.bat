4d6b817f4f52100be9726c0738fb7d7bd29f910a
Chaos Depot <craynafinal@gmail.com>
2019-09-21 14:10:55 -0700

    Add files via upload

---
+++ youtube-music-batch-thumbnail-merge.bat
@@ -0,0 +1,1 @@
+for /f "tokens=1 delims=." %%a in ('dir /B *.mp3') do ffmpeg -loop 1 -i "%%a.png" -i "%%a.mp3" -vf pad="width=ceil(iw/2)*2:height=ceil(ih/2)*2" -c:a copy -c:v libx264 -shortest "%%a.mp4"
